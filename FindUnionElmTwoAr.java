03b45a8d2b83cb28513d3ab45468dc4bfb818931
SaurabhTilak <saurbhtilak6@gmail.com>
2025-09-28 09:27:38 +0530

    Find the union element in two array

---
+++ FindUnionElmTwoAr.java
@@ -0,0 +1,41 @@
+// find the union  elements in two array 
+import java.util.*;
+public class FindUnionElmTwoAr
+{
+	public static void main(String x[])
+	{
+		Scanner s=new Scanner(System.in);
+		int ar[]= {1,2,3,4};
+		int ar2[]= {2,4,5,6,7};
+		int size=ar.length+ar2.length;
+		int c[]=new int[size];
+		for(int i=0;i<ar.length;i++)
+		{
+			c[i]=ar[i];
+		}
+		for(int i=0;i<ar2.length;i++)
+		{
+			c[ar.length+i]=ar2[i];
+		}
+		boolean b[]=new boolean[size];
+		for(int i=0;i<size;i++){
+			if(!b[i])
+			{
+				for(int j=i+1;j<size;j++)
+				{
+					if(c[i]==c[j]){
+					
+						b[j]=true;
+					}
+				}
+			}
+		}
+		System.out.println("Unique elements:");
+		for(int i=0;i<size;i++)
+		{
+			if(!b[i]){
+				System.out.print(c[i]+" ");
+			}
+		}
+	}
+}
